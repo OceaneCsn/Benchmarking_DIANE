8707c207a74ce5f980e4c247c12e8ba244f1154d
Oceane Cassan <oceane.cassan@supagro.fr>
2021-01-29 17:12:21 +0100

    generated all networks and first result plot with validation

---
+++ plotResults.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+
+data <- read.csv("data/resultsBenchmark.csv", sep = ';')
+
+
+ggplot(data, aes(color = method, x = factor(fdr), y = precision)) + 
+  geom_point(size = 2) +
+  facet_wrap(~lfc + density)
+
+
+ggplot(data[data$time > 0,], aes(x = genes, color = factor(density), y = time)) + geom_point(size = 5)
--- validate_networks.R
+++ validate_networks.R
@@ -1,32 +1,120 @@
-net <- read.csv("data/network_edges_d0.03_lfc_1.5.csv")
+library(stringr)
+library(igraph)
 
+# ______ Validation of inferred networks from DIANE's thresholding procedure _________ #
 
-### get list of TFs and target genes
+#' Deals with grouped nodes of a network
+#' 
+#' from mean_ATGSJHKS-ATGODLKSL to ATGDHJKHK 
+#' becomes
+#' from ATGSJHKS to ATGDHJKHK
+#' from ATGODLKSL to ATGDHJKHK
+#' 
+#' @param net network containing grouped regulators,
+#' in the form of a dataframe of edges
+#'
+#' @return dataframe of edges with duplicated edges for grouped regulators
+flatten_edges <- function(net){
+  distinct <- net[!str_detect(net$from, 'mean_') & !str_detect(net$to, 'mean_'),c("from", "to")]
+  grouped <- net[str_detect(net$from, 'mean_') | str_detect(net$to, 'mean_'),]
+  
+  for(i in 1:nrow(grouped)){
+    tf <- grouped[i,"from"]
+    targ <- grouped[i,"to"]
+    if(str_detect(tf, "mean_")){
+      tfs <- unlist(strsplit(stringr::str_remove(tf, "mean_"), '-'))
+    }
+    else
+      tfs <- tf
+    if(str_detect(targ, "mean_")){
+      targs <- unlist(strsplit(stringr::str_remove(targ, "mean_"), '-'))
+    }
+    else 
+      targs <- targ
+    for(tfi in c(tfs)){
+      for(targi in c(targs)){
+        distinct <- rbind.data.frame(distinct, c(tfi, targi))
+      }
+    }
+  }
+  return(distinct)
+}
 
-
-# TFs 
-
-# tfs <- unique(net$from[!stringr::str_detect(net$from, "mean_")])
-# grouped_tfs <- unique(net$from[stringr::str_detect(net$from, "mean_")])
-# ungrouped_tfs <- unique(unlist(strsplit(stringr::str_remove(grouped_tfs, "mean_"), '-')))
-# 
-# regulators <- c(tfs, ungrouped_tfs)
-# 
-# # targets
-# 
-# targets <- unique(net$to[!stringr::str_detect(net$to, "mean_")])
-# grouped_targets <- unique(net$to[stringr::str_detect(net$to, "mean_")])
-# ungrouped_targets <- unique(unlist(strsplit(stringr::str_remove(grouped_targets, "mean_"), '-')))
-# 
-# genes <- c(targets, ungrouped_targets)
-# 
-# 
-# ### get list of TFs and target genes
-# 
-# DEGs <- read.csv("data/DEGs_M-MH.csv", sep = ';')
-# genes <- stringr::str_remove(DEGs$Gene_ID, '\\.[[::digit]]')
-
+# loading edges validated by connecTF database
 connecTF <- read.csv("data/connectf.csv")
 connecTF <- connecTF[,c("gene_id", "TARGET", "TECHNOLOGY.METHOD", "EXPERIMENT_TYPE")]
 validated <- igraph::graph_from_data_frame(connecTF, directed = TRUE, vertices = NULL)
+table(connecTF$TECHNOLOGY.METHOD)
+
+
+# compute fraction of validated edges
+validate_network <- function(fdr = 0.05, density = 0.03, lfc = 1.5, strat = "testing"){
+  if(strat == "testing"){
+    to_read <- paste0("data/network_edges_d", density, "_lfc_", lfc, ".csv")
+    net <- read.csv(to_read)
+    net <- net[net$fdr < fdr,]
+    print(nrow(net))
+  }
+    
+  if(strat == "no_tests"){
+    to_read <- paste0("data/network_edges_d", density, "_lfc_", lfc, "_no_tests.csv")
+    net <- read.csv(to_read)
+  }
+    
+  if(strat == "same_n_edges"){
+    to_read <- paste0("data/network_edges_d", density, "_lfc_", lfc, "_same_n_edges.csv")
+    if(fdr == 0.01)
+      to_read <- paste0("data/network_edges_d", density, "_lfc_", lfc, "_same_n_edges_fdr0.01.csv")
+    net <- read.csv(to_read)
+  }
+  # ungroup grouped regulators to validate individual interactions
+  flat <- flatten_edges(net)
+  pred <- graph_from_data_frame(flat, directed = TRUE, vertices = NULL)
+  
+  # Number of edges for which we have validation data
+  tfs_val <- intersect(flat$from, connecTF$gene_id)
+  validable_edges <- nrow(flat[flat$from %in% tfs_val,])
+  inter <- intersection(pred, validated, keep.all.vertices = TRUE)
+  validated_edges <- gsize(inter)
+  validation_rate <- validated_edges/validable_edges
+  print(paste(validated_edges, "edges were validated over the", validable_edges, "edges that had validation information."))
+  print(paste("Validation rate :", round(validation_rate*100, 2), "%"))
+}
+
+
+validate_network(density = 0.03, lfc = 1.5)
+validate_network(density = 0.03, lfc = 1.5, strat = "no_tests")
+validate_network(density = 0.03, lfc = 1.5, strat = "same_n_edges")
+
+
+validate_network(density = 0.03, lfc = 1.5, fdr = 0.01)
+validate_network(density = 0.03, lfc = 1.5, fdr = 0.01, strat = "same_n_edges")
+
+validate_network(density = 0.01, lfc = 1.5)
+validate_network(density = 0.01, lfc = 1.5, strat = "no_tests")
+validate_network(density = 0.01, lfc = 1.5, strat = "same_n_edges")
+
+validate_network(density = 0.01, lfc = 1.5, fdr = 0.01)
+validate_network(density = 0.01, lfc = 1.5, fdr = 0.01, strat = "same_n_edges")
+
+
+
+
+validate_network(density = 0.03, lfc = 2)
+validate_network(density = 0.03, lfc = 2, strat = "no_tests")
+validate_network(density = 0.03, lfc = 2, strat = "same_n_edges")
+
+
+validate_network(density = 0.03, lfc = 2, fdr = 0.01)
+validate_network(density = 0.03, lfc = 2, fdr = 0.01, strat = "same_n_edges")
+
+validate_network(density = 0.01, lfc = 2)
+validate_network(density = 0.01, lfc = 2, strat = "no_tests")
+validate_network(density = 0.01, lfc = 2, strat = "same_n_edges")
+
+validate_network(density = 0.01, lfc = 2, fdr = 0.01)
+validate_network(density = 0.01, lfc = 2, fdr = 0.01, strat = "same_n_edges")
+
+read.csv("data/network_edges_d_0.03_lfc_1.5_no_tests.csv")
+
 
